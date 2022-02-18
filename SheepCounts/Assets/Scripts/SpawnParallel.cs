f12ed4c9e825535525e5150cbfe83428981a5f75
Dennis Podkolzin <podkolzin@live.ca>
2022-02-18 11:23:45 -0500

    Completed Lesson 10

--- SheepCounts/Assets/Scripts/SpawnParallel.cs
+++ SheepCounts/Assets/Scripts/SpawnParallel.cs
@@ -7,26 +7,48 @@ public class SpawnParallel : MonoBehaviour
 {
     public GameObject sheepPrefab;
     const int numSheep = 15000;
-    List<GameObject> sheepTransforms = new List<GameObject>();
+    List<Transform> sheepTransforms = new List<Transform>();
+
+    struct MoveJob: IJobParallelForTransform
+    {
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.position += 0.1f * (transform.rotation * new Vector3(0,0,1));
+            if(transform.position.z > 50)
+            {
+                transform.position = new Vector3(transform.position.x, 0, -50);
+            }
+        }
+    }
+
+    MoveJob moveJob;
+    JobHandle moveHandle;
+    TransformAccessArray transforms;
+
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < numSheep; i++)
         {
             Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
-            sheepTransforms.Add(Instantiate(sheepPrefab, pos, Quaternion.identity));
+            sheepTransforms.Add(Instantiate(sheepPrefab, pos, Quaternion.identity).transform);
         }
+        transforms = new TransformAccessArray(sheepTransforms.ToArray());
     }
 
     private void Update()
     {
-        foreach(GameObject sheep in sheepTransforms)
-        {
-            sheep.transform.Translate(0, 0, 0.1f);
-            if (sheep.transform.position.z > 50)
-            {
-                sheep.transform.position = new Vector3(sheep.transform.position.x, 0, -50);
-            }
-        }
+        moveJob = new MoveJob { };
+        moveHandle = moveJob.Schedule(transforms);
+    }
+
+    private void LateUpdate()
+    {
+        moveHandle.Complete();
+    }
+    //Jobs stuff has to be disposed of, does not dispose of on its own!
+    private void OnDestroy()
+    {
+        transforms.Dispose();
     }
 }
