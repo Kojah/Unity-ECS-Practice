7d91c9972f2ad147940f90d5d5c4345a963f4f3d
Dennis Podkolzin <podkolzin@live.ca>
2022-02-18 12:28:55 -0500

    Lesson 12 Completed, sheep move

---
+++ SheepCounts/Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities.WithName("MoveSystem").ForEach((ref Translation position, ref Rotation rotation) =>
+        {
+            position.Value += 0.1f * math.forward(rotation.Value);
+            if(position.Value.z > 50)
+            {
+                position.Value.z = -50;
+            }
+        })
+        .Schedule(inputDeps);
+        return jobHandle;
+    }
+}
