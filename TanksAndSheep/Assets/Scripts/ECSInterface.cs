0482a6fc5956399e591311a5fea4e0fe0a2fac9f
Dennis Podkolzin <podkolzin@live.ca>
2022-02-22 14:50:50 -0500

    Lesson 19 Done

---
+++ TanksAndSheep/Assets/Scripts/ECSInterface.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ECSInterface : MonoBehaviour
+{
+    World world; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        world = World.DefaultGameObjectInjectionWorld;
+        Debug.Log($"All entities{world.GetExistingSystem<MoveSystem>().EntityManager.GetAllEntities().Length}");
+
+        EntityManager entityManager = world.GetExistingSystem<MoveSystem>().EntityManager;
+        EntityQuery query = entityManager.CreateEntityQuery(ComponentType.ReadOnly <SheepComponent>());
+        Debug.Log($"All sheep {query.CalculateEntityCount()}");
+    }
+}
