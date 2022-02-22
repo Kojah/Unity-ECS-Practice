c918e2fae3b66cf5cdd381b30f58673f7ad6a317
Dennis Podkolzin <podkolzin@live.ca>
2022-02-22 15:14:08 -0500

    Lesson 20 done

--- TanksAndSheep/Assets/Scripts/ECSInterface.cs
+++ TanksAndSheep/Assets/Scripts/ECSInterface.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using UnityEngine.UI;
+using TMPro;
 
 public class ECSInterface : MonoBehaviour
 {
-    World world; 
+    World world;
 
-
+    [SerializeField] TextMeshProUGUI sheepTextField;
+    [SerializeField] TextMeshProUGUI tonkTextField;
+    EntityManager entityManager;
     // Start is called before the first frame update
     void Start()
     {
+        sheepTextField.SetText("");
+        tonkTextField.SetText("");
         world = World.DefaultGameObjectInjectionWorld;
         Debug.Log($"All entities{world.GetExistingSystem<MoveSystem>().EntityManager.GetAllEntities().Length}");
 
-        EntityManager entityManager = world.GetExistingSystem<MoveSystem>().EntityManager;
-        EntityQuery query = entityManager.CreateEntityQuery(ComponentType.ReadOnly <SheepComponent>());
-        Debug.Log($"All sheep {query.CalculateEntityCount()}");
+        entityManager = world.GetExistingSystem<MoveSystem>().EntityManager;
+        //EntityQuery query = entityManager.CreateEntityQuery(ComponentType.ReadOnly <SheepComponent>());
+        //Debug.Log($"All sheep {query.CalculateEntityCount()}");
+    }
+
+    public void CountSheep()
+    {
+        
+        EntityQuery query = entityManager.CreateEntityQuery(ComponentType.ReadOnly<SheepComponent>());
+        sheepTextField.SetText($"{query.CalculateEntityCount()}");
+    }
+
+    public void CountTonks()
+    {
+
+        EntityQuery query = entityManager.CreateEntityQuery(ComponentType.ReadOnly<TankComponent>());
+        tonkTextField.SetText($"{query.CalculateEntityCount()}");
     }
 }
