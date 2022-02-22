8c7f56c98f78e89e3b042fe3679a1ae4a5af09b6
Dennis Podkolzin <podkolzin@live.ca>
2022-02-22 11:25:13 -0500

    Completed Lesson 17

---
+++ ECS Debugging/Assets/Scripts/MovementSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class MovementSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities.WithName("MoveSystem")
+            .ForEach((ref Translation position, ref Rotation rotations, ref SheepData sheepData) =>
+            {
+                position.Value += 0.01f * math.up();
+            }).Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
