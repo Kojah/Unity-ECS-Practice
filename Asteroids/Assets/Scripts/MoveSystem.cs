edf36e6299462918f37c86517965b44152cc4e57
Dennis Podkolzin <podkolzin@live.ca>
2022-03-07 15:48:00 -0500

    Lesson 28 done

---
+++ Asteroids/Assets/Scripts/MoveSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class NewBehaviourScript : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        float speed = 10.0f;
+        float3 targetLocation = new float3(0, 0, 0);
+
+        var jobHandle = Entities
+            .WithName("MoveSystem")
+            .ForEach((ref Translation position, ref Rotation rotation) =>
+            {
+                float rotSpeed = deltaTime * speed * 1 / math.distance(position.Value, targetLocation);
+                position.Value = math.mul(quaternion.AxisAngle(new float3(0, 1, 0), rotSpeed), position.Value - targetLocation) + targetLocation;
+            })
+            .Schedule(inputDeps);
+
+        return jobHandle; ;
+    }
+}
