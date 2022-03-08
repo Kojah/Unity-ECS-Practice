196843b20a6ac4dd5b03bd64c40bce05bd22221f
Dennis Podkolzin <podkolzin@live.ca>
2022-03-08 10:47:11 -0500

    Lesson 29 done

---
+++ TanksFollow/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotSpeed = 100.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+
+        float rotation = Input.GetAxis("Horizontal") * rotSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
