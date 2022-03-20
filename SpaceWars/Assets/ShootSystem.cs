1e61363d791258a40f36536f6b25544a0d1e6b6c
Dennis Podkolzin <podkolzin@live.ca>
2022-03-20 13:35:36 -0400

    Lesson 36 done

---
+++ SpaceWars/Assets/ShootSystem.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class ShootSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities.WithoutBurst().WithStructuralChanges()
+            .ForEach((Entity entity, ref Translation position, ref Rotation rotation, ref ShipData shipData) =>
+            {
+                var instance = EntityManager.Instantiate(shipData.bulletPrefab);
+                EntityManager.SetComponentData(instance, new Translation { Value = position.Value });
+                EntityManager.SetComponentData(instance, new Rotation { Value = rotation.Value });
+            }).Run();
+
+        return inputDeps;
+    }
+}
