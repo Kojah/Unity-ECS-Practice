0c3dfea482a3c208d9b02f70cda924b8b17b5972
Dennis Podkolzin <podkolzin@live.ca>
2022-03-20 15:15:21 -0400

    Update TimedDestroySystem.cs
    Updated from using the execute model with IJobForEach to something more elegant

--- SpaceWars/Assets/TimedDestroySystem.cs
+++ SpaceWars/Assets/TimedDestroySystem.cs
@@ -7,41 +7,17 @@ using Unity.Collections;
 [UpdateAfter(typeof(MoveBulletSystem))]
 public class TimedDestroySystem : JobComponentSystem
 {
-    EndSimulationEntityCommandBufferSystem buffer;
-
-    protected override void OnCreate()
-    {
-        buffer = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-    }
-
-    struct CullingJob: IJobForEachWithEntity<LifeTimeData>
-    {
-        public EntityCommandBuffer.Concurrent commands;
-        public float dt;
-
-        public void Execute(Entity entity, int index, ref LifeTimeData lifeTimeData)
-        {
-            lifeTimeData.lifeLeft -= dt;
-            if(lifeTimeData.lifeLeft <= 0f)
-            {
-                commands.DestroyEntity(index, entity);
-            }
-        }
-    }
-
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        EntityCommandBuffer.Concurrent commandsECB = buffer.CreateCommandBuffer().ToConcurrent();
-        var job = new CullingJob
-        {
-            commands = commandsECB,
-            dt = Time.DeltaTime
-        };
-
-        var jobHandle = job.Schedule(this, inputDeps);
-        buffer.AddJobHandleForProducer(jobHandle);
-
-        jobHandle.Complete();
+        float dt = Time.DeltaTime;
+        Entities.WithoutBurst().WithStructuralChanges()
+            .ForEach((Entity entity, ref LifeTimeData lifetimeData) =>
+            {
+                lifetimeData.lifeLeft -= dt;
+                if (lifetimeData.lifeLeft <= 0f)
+                    EntityManager.DestroyEntity(entity);
+            })
+        .Run();
 
         return inputDeps;
     }
