b51b8f7f46919541ec2472ab3d45eca4127b99b9
Dennis Podkolzin <podkolzin@live.ca>
2022-03-21 16:31:21 -0400

    Lesson 44 done

---
+++ SpaceWars/Assets/ParticleSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class ParticleSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        JobHandle jobHandle = Entities
+            .WithName("ParticleSystem")
+            .ForEach((ref NonUniformScale scale, ref ParticleData particleData) =>
+            {
+                particleData.timeAlive += deltaTime;
+                scale.Value += particleData.timeAlive * 0.8f;
+                //more complex particle system physics can be done here, you just need position and rotation passed in too
+            })
+            .Schedule(inputDeps);
+
+        jobHandle.Complete();
+
+        return jobHandle;
+    }
+}
